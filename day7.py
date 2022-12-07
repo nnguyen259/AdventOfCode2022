a4c80a9a3d4bbac35ca9b46dd7db1308f72f474d
Nhan Nguyen <Nhan.Nguyen@unb.ca>
2022-12-07 01:00:25 -0500

    AoC2022: Day7

---
+++ day7.py
@@ -0,0 +1,99 @@
+def part1():
+    with open("input/day7.txt") as input:
+        data = input.read().splitlines()
+
+    current_dir = "/"
+    folders = {"/": {"parent": "", "total_size": 0}}
+
+    for line in data:
+        if line.startswith("$"):
+            if line.startswith("$ cd"):
+                _, command, option = line.split(" ")
+                if command == "cd":
+                    if option == "/":
+                        current_dir = "/"
+                    elif option == "..":
+                        current_dir = f"{current_dir.rsplit('/', maxsplit=2)[0]}/"
+                    else:
+                        current_dir += f"{option}/"
+        else:
+            size, name = line.split(" ")
+            new_dir = f"{current_dir}{name}/"
+            if size == "dir":
+                if new_dir not in folders:
+                    folders[new_dir] = {
+                        "parent": current_dir,
+                        "total_size": 0,
+                    }
+                else:
+                    print("weird", line)
+            else:
+                size = int(size)
+                folders[current_dir]["total_size"] += size
+                parent_dir = folders[current_dir]["parent"]
+                while parent_dir != "":
+                    folders[parent_dir]["total_size"] += size
+                    parent_dir = folders[parent_dir]["parent"]
+
+    output = 0
+    for folder in folders.values():
+        if folder["total_size"] <= 100000:
+            output += folder["total_size"]
+    return output
+
+
+def part2():
+    with open("input/day7.txt") as input:
+        data = input.read().splitlines()
+
+    current_dir = "/"
+    folders = {"/": {"parent": "", "total_size": 0}}
+
+    for line in data:
+        if line.startswith("$"):
+            if line.startswith("$ cd"):
+                _, command, option = line.split(" ")
+                if command == "cd":
+                    if option == "/":
+                        current_dir = "/"
+                    elif option == "..":
+                        current_dir = f"{current_dir.rsplit('/', maxsplit=2)[0]}/"
+                    else:
+                        current_dir += f"{option}/"
+        else:
+            size, name = line.split(" ")
+            new_dir = f"{current_dir}{name}/"
+            if size == "dir":
+                if new_dir not in folders:
+                    folders[new_dir] = {
+                        "parent": current_dir,
+                        "total_size": 0,
+                    }
+                else:
+                    print("weird", line)
+            else:
+                size = int(size)
+                folders[current_dir]["total_size"] += size
+                parent_dir = folders[current_dir]["parent"]
+                while parent_dir != "":
+                    folders[parent_dir]["total_size"] += size
+                    parent_dir = folders[parent_dir]["parent"]
+
+    space_left = 70000000 - folders["/"]["total_size"]
+    space_needed = 30000000 - space_left
+
+    current_min_size = folders["/"]["total_size"]
+
+    for folder in folders.values():
+        if (
+            folder["total_size"] >= space_needed
+            and folder["total_size"] <= current_min_size
+        ):
+            current_min_size = folder["total_size"]
+
+    return current_min_size
+
+
+if __name__ == "__main__":
+    print(f"Part1={part1()}")
+    print(f"Part2={part2()}")
