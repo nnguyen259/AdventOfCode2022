3a9e248b12b575981cf114a73db8d964bef9a51c
Nhan Nguyen <Nhan.Nguyen@unb.ca>
2022-12-07 02:05:50 -0500

    AoC2022: Day7-Parsing Revision

--- day7.py
+++ day7.py
@@ -2,43 +2,29 @@ def part1():
     with open("input/day7.txt") as input:
         data = input.read().splitlines()
 
-    current_dir = "/"
-    folders = {"/": {"parent": "", "total_size": 0}}
+    pwd = []
+    sizes = {}
 
     for line in data:
-        if line.startswith("$"):
-            if line.startswith("$ cd"):
-                _, command, option = line.split(" ")
-                if command == "cd":
-                    if option == "/":
-                        current_dir = "/"
-                    elif option == "..":
-                        current_dir = f"{current_dir.rsplit('/', maxsplit=2)[0]}/"
-                    else:
-                        current_dir += f"{option}/"
-        else:
-            size, name = line.split(" ")
-            new_dir = f"{current_dir}{name}/"
-            if size == "dir":
-                if new_dir not in folders:
-                    folders[new_dir] = {
-                        "parent": current_dir,
-                        "total_size": 0,
-                    }
-                else:
-                    print("weird", line)
-            else:
-                size = int(size)
-                folders[current_dir]["total_size"] += size
-                parent_dir = folders[current_dir]["parent"]
-                while parent_dir != "":
-                    folders[parent_dir]["total_size"] += size
-                    parent_dir = folders[parent_dir]["parent"]
+        match line.split(" "):
+            case "$", "cd", "/":
+                pwd = []
+            case "$", "cd", "..":
+                pwd.pop()
+            case "$", "cd", name:
+                pwd.append(name)
+            case "$", "ls":
+                pass
+            case "dir", _:
+                pass
+            case size, _:
+                for i in range(len(pwd)):
+                    folder = ".".join(pwd[0 : i + 1])
+                    if folder not in sizes:
+                        sizes[folder] = 0
+                    sizes[folder] += int(size)
 
-    output = 0
-    for folder in folders.values():
-        if folder["total_size"] <= 100000:
-            output += folder["total_size"]
+    output = sum([x for x in sizes.values() if x < 100000])
     return output
 
 
